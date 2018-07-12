6fa15cdbc1fe165e1d43a79339a9a9e55b8023bf
willsinn <30275717+willsinn@users.noreply.github.com>
2018-07-12 15:42:34 -0400

    setting skill props in Skills to be passed to SkillsChart

--- src/Assets/SkillsChart.jsx
+++ src/Assets/SkillsChart.jsx
@@ -12,22 +12,16 @@ class SkillsChart extends Component {
 
 
   render() {
-    const data01 = [{name: 'Group A', value: 400},
-                    {name: 'Group B', value: 300}
-                  ];
-
-    const data02 = [
-      {name: 'Group C', value: 300},
-      {name: 'Group D', value: 200}
-    ];
+    {props.languages}
+    {props.focus}
 
     return(
 
 
         <ResponsiveContainer width={600} height={600}>
         <PieChart width={730} height={250}>
-          <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-          <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          <Pie data={languages} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+          <Pie data={focus} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
         </PieChart>
         </ResponsiveContainer>
     );
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,20 +5,37 @@ import SkillsChart from '../Assets/SkillsChart';
 
 
 
-class Skills extends Component {
+class SkillsPage extends Component {
 
 
 
   render() {
+    const languages = this.props.languages;
+    const focus = this.props.focus;
+    var name = [];
+    var value = [];
+
+    languages: [
+      name: 'Group A', value: 400,
+      name: 'Group B', value: 300
+    ]
+
+    focus: [
+      name: 'Group C', value: 300,
+      name: 'Group D', value: 200
+    ];
     return (
         <div className="skills-page">
           Skills Page
           <div>
-            <SkillsChart />
+            <SkillsChart
+            {props.languages}
+            {props.focus}
+            />
           </div>
         </div>
     );
   }
 }
 
-export default Skills;
+export default SkillsPage;
