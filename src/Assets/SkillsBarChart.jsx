cba2c2cba2bf807719f72ebdaeec0c16ae616654
willsinn <30275717+willsinn@users.noreply.github.com>
2018-07-13 13:42:57 -0400

    bar chart displays, can't flip axis

--- src/Assets/SkillsBarChart.jsx
+++ src/Assets/SkillsBarChart.jsx
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
-import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from 'recharts';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend,Tooltip } from 'recharts';
 import SkillsStyles from '../Styles/SkillsStyles.css';
 
 
 class SkillsBarChart extends Component {
   render() {
     const focus = [
-      {name: 'Group 1', value: 100},
-      {name: 'Group 2', value: 200},
-      {name: 'Group 3', value: 600},
+      {name: 'Group 1', value: 500},
+      {name: 'Group 2', value: 600},
+      {name: 'Group 3', value: 700},
       {name: 'Group 4', value: 800},
-      {name: 'Group 5', value: 400},
-      {name: 'Group 6', value: 300},
-      {name: 'Group 7', value: 800},
-      {name: 'Group 8', value: 600},
-      {name: 'Group 9', value: 500}
+      {name: 'Group 5', value: 900},
+      {name: 'Group 6', value: 200},
+      {name: 'Group 7', value: 100},
+      {name: 'Group 8', value: 400},
+      {name: 'Group 9', value:  550}
 
     ];
     return(
     <ResponsiveContainer className="bar-container">
-      <BarChart className="bar-chart" layout="vertical" data={focus} barSize={100} barGap={100}>
-
-
-          <Bar dataKey="value" fill="#purple" barGap={100} />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-          <Bar dataKey="value" fill="#82ca9d" />
-
-      </BarChart>
+        <BarChart width={730} height={250} data={focus}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis nameKey="name" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <Legend />
+              <Bar dataKey="value" fill="#8884d8" />
+        </BarChart>
     </ResponsiveContainer>
     );
   }
