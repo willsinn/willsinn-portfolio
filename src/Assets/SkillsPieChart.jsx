ce15f5bd3d4764e94c1138d372fb833e8aae1838
willsinn <30275717+willsinn@users.noreply.github.com>
2018-07-13 15:27:01 -0400

    added progress table, need to resolve component did mount

--- src/Assets/SkillsPieChart.jsx
+++ src/Assets/SkillsPieChart.jsx
@@ -10,7 +10,7 @@ class SkillsPieChart extends Component {
 
 
   render() {
-    const languages = [{name: 'JavaScript', value: 500},
+    const web = [{name: 'JavaScript', value: 500},
                     {name: 'CSS StylesSheets', value: 150},
                     {name: 'HTML', value: 150}
                   ];
@@ -25,7 +25,7 @@ class SkillsPieChart extends Component {
 
         <ResponsiveContainer className="pie-container">
         <PieChart>
-          <Pie data={languages} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label />
+          <Pie data={web} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={100} fill="#8884d8" label />
           <Pie data={focus} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={110} outerRadius={140} fill="#82ca9d" label />
         </PieChart>
         </ResponsiveContainer>
--- src/Components/SkillsPage.jsx
+++ src/Components/SkillsPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SkillsStyles from '../Styles/SkillsStyles.css';
 import SkillsPieChart from '../Assets/SkillsPieChart';
 import SkillsBarChart from '../Assets/SkillsBarChart';
+import SkillsProgressChart from '../Assets/SkillsProgressChart';
 
 
 
@@ -12,11 +13,13 @@ class SkillsPage extends Component {
 
 
   render() {
+
     return (
         <div className="skills-page">
           Skills Page
+
             <SkillsPieChart />
-            <SkillsBarChart />
+            <SkillsProgressChart />
         </div>
     );
   }
