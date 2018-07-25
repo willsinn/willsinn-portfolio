dfda1983e1abaf9239b7ba79e86c7c3da3d59cf7
willsinn <30275717+willsinn@users.noreply.github.com>
2018-07-25 05:10:12 -0400

    refactored LandingStyles into LandingPage, fixed background image underlay and ContactButton position

--- src/Assets/ContactFormDrawer.jsx
+++ src/Assets/ContactFormDrawer.jsx
@@ -23,33 +23,33 @@ class ContactFormDrawer extends Component {
     const { classes } = this.props;
 
     const sideForm = (
-      <form  className={classes.formContainer} onSubmit="onSubmit">
+      <div className={classes.formContainer}>
+      <form onSubmit="onSubmit">
       <ul className={classes.inputListWrapper}>
 
-        <Paper>
         <li> What should I call you?* </li>
 
         <li><input type="text" name="firstName"  />
             <input type="text"  name="lastName" /></li>
 
         <li>First       Last</li>
-        </Paper>
         <li>Email</li>
         <li><input type="email" name="user_email"  /></li>
 
         <li>Phone</li>
         <li>
-          1 - <input type="text" name="phone" pattern="[0-9]{3}" title="areaCode" min="3" max="3"  />
-          - <input type="text" name="phone" pattern="[0-9]{3}" title="firstThreeDigits" min="3" max="3" />
-          - <input type="text" name="phone" pattern="[0-9]{4}" title="lastFourDigits" min="4" max="4"/>
+          1-<input className={classes.phoneInput} type="text" name="phone" pattern="[0-9]{3}" title="areaCode" min="3" max="3"  />
+          -<input className={classes.phoneInput} type="text" name="phone" pattern="[0-9]{3}" title="firstThreeDigits" min="3" max="3" />
+          -<input className={classes.phoneInput} type="text" name="phone" pattern="[0-9]{4}" title="lastFourDigits" min="4" max="4"/>
         </li>
 
-        <li><textarea name="message" rows="10" cols="50">
+        <li><textarea name="message" rows="10" cols="120">
             </textarea></li>
         <input type="Submit" value="submit" />
 
         </ul>
         </form>
+        </div>
 
     );
 
@@ -57,15 +57,15 @@ class ContactFormDrawer extends Component {
 
     return (
       <div className={classes.root}>
-      <button
+        <button
          className={classes.contactButton}
-        onClick={this.toggleDrawer('right', true)}
-      >
-         <Paper className={classes.contactButton}>
-        <i class="material-icons">email</i>
-        Contact Me
-        </Paper>
-      </button>
+         onClick={this.toggleDrawer('right', true)}
+        >
+          <Paper className={classes.contactButton}>
+          <i class="material-icons">email</i>
+          Contact Me
+          </Paper>
+        </button>
         <SwipeableDrawer
           className={classes.formDrawerContainer}
           anchor='right'
@@ -94,10 +94,10 @@ const styles = theme => ({
   root:  {
     display: 'flex',
     flexDirection: 'column',
-    position: 'relative',
-
   },
   formDrawerContainer: {
+
+
 
   },
 
@@ -105,10 +105,16 @@ const styles = theme => ({
 
   },
   formContainer: {
+    backgroundColor:'gray',
+    width: '100%',
   },
   inputListWrapper: {
     listStyle: 'none',
+    padding: '1px',
   },
+  phoneInput: {
+    width: '30px',
+  }
 
 
 
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Paper, SwipeableDrawer } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import LandingStyles from '../Styles/LandingStyles.css';
 import AbsoluteProfile from '../Images/AbsoluteProfile.jpeg';
 import ContactFormDrawer from '../Assets/ContactFormDrawer';
 
@@ -10,72 +8,39 @@ import ContactFormDrawer from '../Assets/ContactFormDrawer';
 
 class LandingPage extends Component {
 
-  state = {
-    right: false,
-  };
-  toggleDrawer = (side, open) => () => {
-    this.setState({
-      [side]: open,
-    });
-  };
 
   render() {
     const { classes } = this.props;
 
     return (
         <div className={classes.root}>
-
          <img className={classes.backgroundImage} src={AbsoluteProfile}/>
-          <ContactFormDrawer />
+        <ContactFormDrawer className={classes.contactButton} />
         </div>
     );
   }
 }
 const styles = theme => ({
   root: {
+    height: '100%',
+    width: '100%',
+    zIndex: '0',
+    opacity: '0.5',
+  },
+
+
+  backgroundImage: {
+    zIndex: '-1',
+    backgroundImage: 'cover',
+    width: '100%',
+    height: '100%',
+    opactity: '0.3',
     position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflow: 'hidden',
-  },
-
-  mainPage: {
-    zIndex: '1'
 
   },
-  rightPage: {
-    zIndex: 2,
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    transition: 'transform .3s ease-out',
-    WebkitTransition: '-webkit-transform .3s ease-out',
-    willChange: 'transform',
-    overflowY: 'auto',
 
-  },
-  formDrawerContainer: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    overflowY: 'scroll',
-    WebkitOverflowScrolling: 'touch',
-    transition: 'left .3s ease-out, right .3s ease-out',
-  },
-
-
-  contactButton: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-
-  },
-  closeDrawerButton: {},
-
+    contactButton: {
+    },
 
 });
 
