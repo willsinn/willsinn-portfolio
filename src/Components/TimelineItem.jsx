14c0ebf9c12380112c5b06567586c9b95d9f0a95
willsinn <30275717+willsinn@users.noreply.github.com>
2018-07-26 20:02:27 -0400

    must create Timeline Component to pass props properly

--- src/Components/TimelineItem.jsx
+++ src/Components/TimelineItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import 
 
 
 
@@ -7,39 +8,59 @@ class TimelineItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: {},
+      title:'',
+      id: [1,2,3,4],
+      text: '',
+      items: []
       }
     }
 
-renderItem = (props) => {
-  const { content } = props;
-  return(<text>{content}</text>);
-}
-
-
+  postItems(props) {
+    const header = props.items.map((item) =>
+    <div key={item.id}>
+    <h3>{item.title}</h3>
+    </div>
+  );
+    const content = props.items.map((item) =>
+    <div key={item.id}>
+      <p>{item.text}</p>
+    </div>
+   );
+   return(
+     <div>
+     {header}
+     <hr/>
+     {content}
+     </div>
+   );
+  }
 
 
   render() {
     const { classes } = this.props;
-    const renderItem = (props) => {
-    const header = [
-      {content: 'Title1'},
-      {content: 'Title2'}
+
+    const items = [
+      { id: 1, title: 'Title1', text: 'ThisIsTitle1'},
+      { id: 2, title: 'Title2', text: 'ThisIsTitle2'},
+      { id: 3, title:'Body1', text:'ThisIsBody1'},
+      { id: 4, title:'Body2', text:'ThisIsBody2'},
     ];
-    const body = [
-      {content: 'Body1'},
-      {content: 'Body2'},
-    ];
-    const items = { header, body };
-  };
+    const content = {props.items.map((item) =>
+    <Content
+      key={item.id}
+      id={item.id}
+      text={item.text}
+    />
+  });
+    const Content = this.props.items;
 
     return(
       <div className={classes.root}>
         <div className={classes.timelineContainer}>
 
             <div className={classes.timelineHeader}>
-            <text
-              {this.props.renderItem(header, content)}>
+            <li>{this.props.header}</li>
+            </div>
 
             <div className={classes.timelineBody}>
             </div>
