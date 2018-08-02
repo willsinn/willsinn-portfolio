177152ac2720750126fdc556bf83c4892c3b720b
willsinn <30275717+willsinn@users.noreply.github.com>
2018-08-02 14:44:59 -0400

    resized maps perfectly, styled and repositioned content accordingly,  added View Recent Desgins Button

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -25,14 +25,17 @@ class AboutSection extends Component {
           </div>
           <div className={classes.aboutContent}>
             <div className={classes.contentColumnState1}>
-              <p> Born in Lafayette and raised in Baton Rouge where I attended LSU and graduated from ULL in 3D animation. I visit New Orleans as much as possible and am a Tiger fan. I will always choose Cajun food over Creole.</p>
+              <p className={classes.columnStateText}> Born in Lafayette and raised in Baton Rouge where I attended LSU and graduated from ULL in 3D animation. I visit New Orleans as much as possible and am a Tiger fan. I will always choose Cajun food over Creole.</p>
             </div>
             <div className={classes.contentColumnState2}>
-              <p>I moved to Austin in 2003 with no job and started a web design firm. I met my lovely wife one year later and we’ve grown our businesses and lives together in ATX. We now live in the area of Dirty Six in downtown. Ive become a BBQ and taco snob.</p>
+              <p className={classes.columnStateText}>I moved to Austin in 2003 with no job and started a web design firm. I met my lovely wife one year later and we’ve grown our businesses and lives together in ATX. We now live in the area of Dirty Six in downtown. Ive become a BBQ and taco snob.</p>
             </div>
             <div className={classes.contentColumnState3}>
-              <p>I first travelled to Cali in 2010 for a Joomla convention. A year and half later I was hired by eBay to head up the UX for a massive analytics portal. After that I moved on to a Sears analytics team also in CA. Something tells me Ill be back...</p>
+              <p className={classes.columnStateText}>I first travelled to Cali in 2010 for a Joomla convention. A year and half later I was hired by eBay to head up the UX for a massive analytics portal. After that I moved on to a Sears analytics team also in CA. Something tells me Ill be back...</p>
             </div>
+          </div>
+          <div className={classes.aboutButtonWrapper}>
+            <button className={classes.aboutViewDesignsButton} onClick> View Recent Designs </button>
           </div>
          </div>
         </div>
@@ -43,7 +46,6 @@ class AboutSection extends Component {
 const styles = theme => ({
   root: {
     backgroundColor: 'gray',
-
   },
   aboutWrapper: {
     display: 'flex',
@@ -53,39 +55,64 @@ const styles = theme => ({
   aboutContainer: {
       display: 'flex',
       flexDirection: 'column',
-      width: '60%',
-
+      width: '65%',
+      alignItems: 'center',
   },
   aboutIntroduction: {
+    paddingTop: '10%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-
   },
   aboutContent: {
     display: 'flex',
-    alignItems: 'center'
-
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-
   contentColumnState1: {
-    width: '33%',
-    backgroundImage: `url(${ CA })`,
-    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '30%',
+    height: '400px',
+    backgroundImage: `url(${ LA })`,
+    backgroundSize: '300px 400px',
+    backgroundRepeat: 'no-repeat',
 
   },
   contentColumnState2: {
-    width: '33%',
-    backgroundImage: `url(${ LA })`,
-    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '30%',
+    height: '400px',
+    backgroundImage: `url(${ TX })`,
+    backgroundSize: '300px 400px',
+    backgroundRepeat: 'no-repeat',
 
   },
   contentColumnState3: {
-    width: '33%',
-    backgroundImage: `url(${ TX })`,
-    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '30%',
+    height: '400px',
+    backgroundImage: `url(${ CA })`,
+    backgroundSize: '300px 400px',
+    backgroundRepeat: 'no-repeat',
 
+  },
+  columnStateText: {
+    color: 'white',
+    fontSize: '18px',
+    width: '70%',
+  },
+  aboutButtonWrapper: {
+    paddingTop: '10%',
+  },
+  aboutViewDesignsButton: {
+    fontSize: '22px',
   },
 });
 
