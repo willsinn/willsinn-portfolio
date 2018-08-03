36914808f33adfd60c0a0d267fa258fb4194b952
willsinn <30275717+willsinn@users.noreply.github.com>
2018-08-03 00:26:18 -0400

    added Work Section title, styled same as About title

--- src/Components/WorkSection.jsx
+++ src/Components/WorkSection.jsx
@@ -9,7 +9,13 @@ class WorkSection extends Component {
 
     return (
         <section className={classes.root}>
-          <h1>WorkSection Header</h1>
+          <div className={classes.titleContainer}>
+          <div className={classes.beforeTitle}>
+          _______________</div>
+            <h1 className={classes.workTitle}>WORK</h1>
+          <div className={classes.afterTitle}>
+          _______________</div>
+          </div>
           <TimelineList
           />
         </section>
@@ -19,7 +25,27 @@ class WorkSection extends Component {
 const styles = theme => ({
   root: {
     width: '100%',
-    backgroundColor: 'yellow',
+    backgroundColor: 'orange',
+  },
+  titleContainer: {
+    paddingTop: '5%',
+    margin: '0',
+    fontSize: '36px',
+    fontWeight: '600',
+    color: 'white',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  beforeTitle: {
+    paddingRight: '20%',
+    marginBottom: '4%',
+  },
+  workTitle: {},
+  afterTitle: {
+    paddingLeft: '20%',
+    marginBottom: '4%',
   },
 });
 export default withStyles(styles, {})(WorkSection);
