87a982748823901622c77496891295b7ff6ac29a
willsinn <30275717+willsinn@users.noreply.github.com>
2018-08-09 16:03:00 -0400

    added static labels that inherit color and payload. Renders payload with pointer tracing back to source and converts value of payload to percentage respectively

--- src/Assets/SkillsPieChart.jsx
+++ src/Assets/SkillsPieChart.jsx
@@ -83,7 +83,7 @@ const { value, percent, x, y, midAngle } = props;
         />
         <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
         <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
-        <text className="hover-text-name" x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={fill} >
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={fill} >
           {payload.name}
         </text>
         <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill={fill}>
@@ -108,16 +108,48 @@ const { value, percent, x, y, midAngle } = props;
       </text>
     );
     };
-    const outerStaticLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index, payload }) => {
+    const outerStaticLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+      fill, payload, percent, index }) => {
       const RADIAN = Math.PI / 180;
  	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x  = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+  const sin = Math.sin(-RADIAN * midAngle);
   const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? 'start' : 'end';
+
   return (
-    <text x={x + (cos >= 0 ? 1 : -1) * 12} y={y} fill="white" textAnchor={x} 	dominantBaseline="central">
-    	{payload.name}:{`${(percent * 100).toFixed(0)}%`}
-    </text>
+    <g>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius}
+        fill={fill}
+      />
+      <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none"/>
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none"/>
+      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={fill} >
+        {payload.name}:{`${(percent * 100).toFixed(0)}%`}
+      </text>
+    </g>
   );
 };
 
