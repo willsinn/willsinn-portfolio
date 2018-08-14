63feeda66d0eea60553e7ddf0a5b574b55f57a56
willsinn <30275717+willsinn@users.noreply.github.com>
2018-08-14 12:37:24 -0400

    set up component layout for inner pie of Pie Chart

--- src/Assets/InnerPie.jsx
+++ src/Assets/InnerPie.jsx
@@ -1,35 +1,18 @@
-const renderActiveInner = (props) => {
-  const { cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle, fill, payload, percent } = props;
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cx + radius * Math.sin(-midAngle * RADIAN);
-  const textAnchor = x > cx ? 'start' : 'end';
-  return (
-        <g>
-    <Sector
-      cx={cx}
-      cy={cy}
-      outerRadius={outerRadius}
-      startAngle={startAngle}
-      endAngle={endAngle}
-      fill={fill}
-    />
-    <Sector
-      cx={cx}
-      cy={cy}
-      startAngle={startAngle}
-      endAngle={endAngle}
-      outerRadius={outerRadius}
-      fill={fill}
-    />
+import React, { Component } from 'react';
+import { ResponsiveContainer, PieChart, Pie, Cell, Sector } from 'recharts';
+import { withStyles } from '@material-ui/core/styles';
 
-  <path d={`${x},${y}`} stroke={fill} fill="none" />
-      <text className="hover-text-name" x={x} y={y} textAnchor={textAnchor} fill="white" >
-        {payload.name}
-        {`${(percent * 100).toFixed(2)}%`}
-        {`${payload.value}`}
-        Inner
-      </text>
-    </g>
-  );
-};
+
+class InnerPie extends Component {
+  render()
+  const { classes } = this.props;
+    return(
+      <ResponsiveContainer className={classes.root}>
+      </ResponsiveContainer>
+    );
+
+}
+
+const styles = theme => ({});
+
+export default withStyles(styles, {})(InnerPie);
