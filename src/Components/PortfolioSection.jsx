a28c199ab0ba0c4c32d6204eb71bc7f9917f78b6
willsinn <30275717+willsinn@users.noreply.github.com>
2018-08-20 14:12:06 -0400

    got Portfolio section tabs working, work on styling portfolio tabs respective to projects.

--- src/Components/PortfolioSection.jsx
+++ src/Components/PortfolioSection.jsx
@@ -1,79 +1,65 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { Card, CardHeader, CardContent, Typography, Tabs, Tab, Paper, Avatar, CardMedia } from '@material-ui/core';
+import { Card, CardHeader, CardContent, Typography, Tabs, Tab, Paper, Avatar, CardMedia, AppBar,  } from '@material-ui/core';
 
 
 class PortfolioSection extends Component {
-  constructor() {
-    super();
-    this.state = { activeTab:0 };
-  }
-  handleChange = (event, value) => {
-    this.setState({ activeTab : value });
-  };
-  toggleProjects() {
-    return (
-      <Card>
-          <CardHeader
-            avatar={
-              <Avatar aria-label="Recipe">
-                R
-              </Avatar>
-            }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
-          />
-          <CardMedia
+    state = {
+      activeTab : 0
+    };
+    handleChange = (event, activeTab) => {
+       this.setState({ activeTab });
+     };
 
-            image="/static/images/cards/paella.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography component="p">
-              This impressive paella is a perfect party dish and a fun meal to cook together with
-              your guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-          </CardContent>
-          <button>
-            View Code
-          </button>
-          <button>
-            Try it Out
-          </button>
-      </Card>
-          );
-          if(this.state.activeTab === 0){
-            return ( <Card>proj2</Card> )
-          } else if (this.state.activeTab === 1){
-            return ( <Card>proj3</Card> )
-          } else if (this.state.activeTab === 2){
-            return ( <Card>proj4</Card> )
-          }
-        }
-        render() {
-        const { classes } = this.props;
+     render() {
+       const { classes } = this.props;
+       const { activeTab } = this.state;
 
-        return(
-          <section className={classes.root}>
-            <Paper>
-              <Tabs
-                value={this.state.activeTab}
-                onChange={this.handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-                >
-                  <Tab label="Item One" />
-                  <Tab label="Item Two" />
-                  <Tab label="Item Three" />
-                  <Tab label="Item Four" />
-              </Tabs>
-            </Paper>
-            <Paper className="content">{this.toggleProjects()}</Paper>
-          </section>
-      );
-  }
-}
+       return (
+         <div className={classes.root}>
+           <AppBar position="static">
+             <Tabs activeTab={activeTab}
+                  onChange={this.handleChange}>
+               <Tab label="Item One" />
+               <Tab label="Item Two" />
+               <Tab label="Item Three" href="#basic-tabs" />
+             </Tabs>
+           </AppBar>
+           {activeTab === 0 && <Card>
+               <CardHeader
+                 avatar={
+                   <Avatar aria-label="Recipe">
+                     R
+                   </Avatar>
+                 }
+                 title="Shrimp and Chorizo Paella"
+                 subheader="September 14, 2016"
+               />
+               <CardMedia
+
+                 image="/static/images/cards/paella.jpg"
+                 title="Contemplative Reptile"
+               />
+               <CardContent>
+                 <Typography component="p">
+                   This impressive paella is a perfect party dish and a fun meal to cook together with
+                   your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                 </Typography>
+               </CardContent>
+               <button>
+                 View Code
+               </button>
+               <button>
+                 Try it Out
+               </button>
+           </Card>}
+           {activeTab === 1 && <Card>Item Two</Card>}
+           {activeTab === 2 && <Card>Item Three</Card>}
+         </div>
+       );
+     }
+   }
+
 const styles = theme => ({
   root: {
   },
