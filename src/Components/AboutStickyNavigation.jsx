f8f54b39cc98a5153d069857ff5a1f2c2fca136a
willsinn <30275717+willsinn@users.noreply.github.com>
2018-09-10 11:38:29 -0400

    removed state in Sticky Nav, downloading react-router for StickyNav and URL routing.

--- src/Components/AboutStickyNavigation.jsx
+++ src/Components/AboutStickyNavigation.jsx
@@ -4,27 +4,17 @@ import { Tab, Tabs, AppBar } from '@material-ui/core';
 
 
 class AboutStickyNavigation extends Component {
-    state = {
-      activeSection : 0
-    };
-    handleChange = (event, activeSection) => {
-       this.setState({ activeSection });
-    };
-
     render() {
       const { classes } = this.props;
-      const { activeSection } = this.state;
 
         return (
        <div className={classes.root}>
+
           <AppBar>
-              <Tabs
-              activeSection={activeSection}
-              onChange={this.handleChange}
-              >
+              <Tabs>
                 <Tab label="About" />
                 <Tab label="Portfolio" />
-                <Tab label="Skills" href="#basic-tabs" />
+                <Tab label="Skills" />
                 <Tab label="Work" />
                 <Tab label="Contact" />
               </Tabs>
