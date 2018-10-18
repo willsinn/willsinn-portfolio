e7958f58d1255e70f6f8e1cc263f887d956cdb8b
willsinn <30275717+willsinn@users.noreply.github.com>
2018-10-18 12:17:49 -0400

    adapted classes and refs tomy app, scroll-to links work

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import SkillsSection from './Components/SkillsSection';
 import WorkSection from './Components/WorkSection';
 import ContactSection from './Components/ContactSection';
 import { withStyles } from '@material-ui/core/styles';
-import StickyNavBar from './Components/StickyNavBar';
 import scrollToComponent from 'react-scroll-to-component';
 import { AppBar } from '@material-ui/core';
 
@@ -18,11 +17,11 @@ class App extends Component {
 }
 
 componentDidMount() {
-  scrollToComponent(this.Blue, { offset: 0, align: 'middle', duration: 500, ease:'inCirc'});
+  scrollToComponent(this.Hero, { offset: 0, align: 'middle', duration: 500, ease:'inCirc'});
 }
 
 scrollToTopWithCallback() {
-  let scroller = scrollToComponent(this.Violet, { offset: 0, align: 'top', duration: 1500});
+  let scroller = scrollToComponent(this.Hero, { offset: 0, align: 'top', duration: 1500});
   scroller.on('end', () => console.log('Scrolling end!') );
 }
   render() {
@@ -32,24 +31,17 @@ scrollToTopWithCallback() {
       <section className={classes.root}>
       <div className='main'>
         <AppBar className='button_group'>
-          <button onClick={this.scrollToTopWithCallback}>Go To Violet</button>
-          <button onClick={() => scrollToComponent(this.Violet, { offset: 0, align: 'top', duration: 1500})}>Go To Violet</button>
-          <button onClick={() => scrollToComponent(this.Indigo, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}>Go To Indigo</button>
-          <button onClick={() => scrollToComponent(this.Blue, { offset: -200, align: 'middle', duration: 1500, ease:'inCirc'})}>Go To Blue</button>
-          <button onClick={() => scrollToComponent(this.Green, { offset: 0, align: 'middle', duration: 500, ease:'inExpo'})}>Go To Green</button>
-          <button onClick={() => scrollToComponent(this.Yellow, { offset: 0, align: 'top', duration: 1500, ease:'inCirc'})}>Go To Yellow</button>
-          <button onClick={() => scrollToComponent(this.Orange, { offset: 0, align: 'top', duration: 500, ease:'inCirc'})}>Go To Orange</button>
-          <button onClick={() => scrollToComponent(this.Red, { offset: 0, align: 'top', duration: 500})}>Go To Red</button>
-
+          <button onClick={this.scrollToTopWithCallback}>Will Sinn</button>
+          <button onClick={() => scrollToComponent(this.About, { offset: 0, align: 'bottom', duration: 500, ease:'inExpo'})}>About</button>
+          <button onClick={() => scrollToComponent(this.Portfolio, { offset: -200, align: 'middle', duration: 1500, ease:'inCirc'})}>Portfolio</button>
+          <button onClick={() => scrollToComponent(this.Skills, { offset: 0, align: 'middle', duration: 500, ease:'inExpo'})}>Skills</button>
+          <button onClick={() => scrollToComponent(this.Work, { offset: 0, align: 'top', duration: 1500, ease:'inCirc'})}>Work</button>
         </AppBar>
-        <section className='hero' ref={(section) => { this.Violet = section; }}><HeroSection /></section>
-        <section className='indigo' ref={(section) => { this.Indigo = section; }}><AboutSection /></section>
-        <section className='blue' ref={(section) => { this.Blue = section; }}><PortfolioSection /></section>
-        <section className='green' ref={(section) => { this.Green = section; }}><SkillsSection /></section>
-        <section className='yellow' ref={(section) => { this.Yellow = section; }}><WorkSection /></section>
-        <section className='orange' ref={(section) => { this.Orange = section; }}><ContactSection /></section>
-        <section className='red' ref={(section) => { this.Red = section; }}>Red</section>
-        <b>Inspired By <a href="https://hopechen1028.github.io/hopechen.me/" target="_blank">Hope</a> with Love and Peace</b>
+        <section className='hero' ref={(section) => { this.Hero = section; }}><HeroSection /></section>
+        <section className='about' ref={(section) => { this.About = section; }}><AboutSection /></section>
+        <section className='blue' ref={(section) => { this.Portfolio = section; }}><PortfolioSection /></section>
+        <section className='green' ref={(section) => { this.Skills = section; }}><SkillsSection /></section>
+        <section className='yellow' ref={(section) => { this.Work = section; }}><WorkSection /></section>
       </div>
 
 
